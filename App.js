2bc86ba33f221644e8ec4ae86c1bf777314e229f
Victor Velasquez <victorv24@gmail.com>
2018-09-06 11:33:27 -0700

    adding React Navigation

--- App.js
+++ App.js
@@ -1,15 +1,26 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react'
+import { StyleSheet, 
+  Text, 
+  View } from 'react-native'
+  import { createStackNavigator } from 'react-navigation'
+
+class Greeting extends Component {
+  render() {
+    return (
+      <Text>Hello {this.props.name}!</Text>
+    )
+  }
+}
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Greeting name='Rexxar' />
+        <Greeting name='Jaina' />
+        <Greeting name='Valeera' />
       </View>
-    );
+    )
   }
 }
 
@@ -20,4 +31,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
