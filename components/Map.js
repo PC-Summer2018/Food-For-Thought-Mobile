251e72e8fdb56354e8d6d1b4f4bb860d1d302895
Victor Velasquez <victorv24@gmail.com>
2018-09-09 12:30:14 -0700

    typos and pattern changes

--- components/Map.js
+++ components/Map.js
@@ -12,30 +12,34 @@ import {
 import MapView, { Marker } from 'react-native-maps'
 import MapViewDirections from 'react-native-maps-directions'
 
-// FOR TESTING ROUTE NAVIGATION
-const coordinates = [
-	{
-		latitude: 36.1583869,
-    longitude: -115.152501
-	},
-	{
-		latitude: 36.117388,
-    longitude: -115.169968
-	}
-]
-
 const GOOGLE_MAPS_APIKEY = 'AIzaSyDNIsEsuc8FsHQJsswUcDKUd9k3sZqzk3U'
 
 class Map extends Component {
 	state = {
-		currentLatLng: {
-			latitude: null,
-			longitude: null,
-			error: null
+		region: {
+			latitude: 36.1699412,
+      longitude: -115.1398295,
+      latitudeDelta: 0.5,
+      longitudeDelta: 0.0421
 		}
 	}
 
-	showCurrentLocation() {
+	// showCurrentLocation() {
+	// 	this.watchId = navigator.geolocation.watchPosition(
+	// 		(position) => {
+	// 			this.setState({
+	// 				latitude: position.coords.latitude,
+	// 				longitude: position.coords.longitude,
+	// 				error: null
+	// 			})
+	// 		},
+	// 		(error) => this.setState({ error: error.message }),
+	// 		{ enableHighAccuracy: true, timeout: 20000, maximumAge: 1000, distanceFilter: 10 }
+	// 	)
+	// }
+
+	componentDidMount() {
+		// this.showCurrentLocation()
 		this.watchId = navigator.geolocation.watchPosition(
 			(position) => {
 				this.setState({
@@ -49,71 +53,57 @@ class Map extends Component {
 		)
 	}
 
-	componentDidMount() {
-		this.showCurrentLocation()
-		
-		// TEST: Using getCurrentPosition
-		// navigator.geolocation.getCurrentPosition(
-		// 	(position) => {
-		// 		this.setState({
-		// 			latitude: position.coords.latitude,
-		// 			longitude: position.coords.longitude,
-		// 			error: null
-		// 		})
-		// 	},
-		// 	(error) => this.setState({ error: error.message }),
-		// 	{ enableHighAccuracy: true, timeout: 2000, maximumAge: 1000 }
-		// )
-
-		// TEST: Using watchPosition
-		// this.watchId = navigator.geolocation.watchPosition(
-		// 	(position) => {
-		// 		this.setState({
-		// 			latitude: position.coords.latitude,
-		// 			longitude: position.coords.longitude,
-		// 			error: null
-		// 		})
-		// 	},
-		// 	(error) => this.setState({ error: error.message }),
-		// 	{ enableHighAccuracy: true, timeout: 20000, maximumAge: 1000, distanceFilter: 10 }
-		// )
-	}
-
 	componentWillUnmount() {
 		navigator.geolocation.clearWatch(this.watchId)
 	}
 
 	render() {
+
 		return (
 			<View style={styles.container}>
 				<MapView style={styles.map}
-			    initialRegion={{
-			      latitude: 36.1699,
-			      longitude: -115.1398,
-			      latitudeDelta: 0.0922,
-			      longitudeDelta: 0.0421,
-			    }}>
+			    initialRegion={this.state.region}
+					followsUserLocation: {true}
+					showsMyLocationButton: {true}
+					showsCompass: {true}>
 
 				  <MapView.Marker
-						coordinate={coordinates[0]}
+						coordinate={{
+							latitude: this.state.latitude,
+							longitude: this.state.longitude
+						}}
 						title={'Current location'}
 						description={'A school for gifted children.'}
 				  />
 				  <MapView.Marker
-						coordinate={coordinates[1]}
+						coordinate={{
+							latitude: 36.117388,
+    					longitude: -115.169968
+						}}
 						title={'In-N-Out Burger'}
 						description={'The food of the people.'}
 				  />
 
 				  <MapViewDirections 
-				  	origin={coordinates[0]}
-				  	destination={coordinates[1]}
+				  	origin={{
+				  		latitude: this.state.latitude,
+							longitude: this.state.longitude
+				  	}}
+				  	destination={{
+				  		latitude: 36.117388,
+    					longitude: -115.169968
+				  	}}
 				  	apikey={GOOGLE_MAPS_APIKEY}
 				  	strokeWidth={3}
 				  	strokeColor='blue'
 				  	language='en'
 				  />
 		  	</MapView>
+
+		  	<Text>Latitude: {this.state.latitude}</Text>
+		  	<Text>Latitude: {this.state.longitude}</Text>
+		  	{this.state.error ? <Text>Error: {this.state.error}</Text> : null}
+				
 				<Text>French Toast</Text>
 				<Text>Potatoes</Text>
 				<Text>French Fries</Text>
