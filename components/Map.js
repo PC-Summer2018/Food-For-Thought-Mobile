313591a2113ed6bf89071f7ce42031d82a088fe9
Victor Velasquez <victorv24@gmail.com>
2018-09-08 19:14:44 -0700

    fake data on the screen

--- components/Map.js
+++ components/Map.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react'
-import { View,  Text, StyleSheet } from 'react-native'
+import { 
+	AppRegistry, 
+	StyleSheet, 
+	Dimensions, 
+	Image, 
+	View, 
+	Text,
+	StatusBar, 
+	TouchableOpacity 
+} from "react-native";
 import MapView, { Marker } from 'react-native-maps'
 import MapViewDirections from 'react-native-maps-directions'
-// import { compose, withProps, lifecycle } from 'recompose'
 
+// FOR TESTING ROUTE NAVIGATION
 const coordinates = [
 	{
 		latitude: 36.1583869,
@@ -18,74 +27,63 @@ const coordinates = [
 const GOOGLE_MAPS_APIKEY = 'AIzaSyDNIsEsuc8FsHQJsswUcDKUd9k3sZqzk3U'
 
 class Map extends Component {
-	// state = {
-	// 	currentLatLng: {
-	// 		lat: 0,
-	// 		lng: 0
-	// 	},
-	// 	// rte: []
-	// }
+	state = {
+		currentLatLng: {
+			latitude: null,
+			longitude: null,
+			error: null
+		}
+	}
 
-	// showCurrentLocation() {
-	// 	if (navigator.geolocation) {
-	// 		navigator.geolocation.getCurrentPosition(position => {
-	// 			this.setState({
-	// 				currentLatLng: {
-	// 					lat: position.coords.latitude,
-	// 					lng: position.coords.longitude
-	// 				}
-	// 			})
-	// 		})
-	// 	}
-	// }
+	showCurrentLocation() {
+		this.watchId = navigator.geolocation.watchPosition(
+			(position) => {
+				this.setState({
+					latitude: position.coords.latitude,
+					longitude: position.coords.longitude,
+					error: null
+				})
+			},
+			(error) => this.setState({ error: error.message }),
+			{ enableHighAccuracy: true, timeout: 20000, maximumAge: 1000, distanceFilter: 10 }
+		)
+	}
 
 	componentDidMount() {
-		// this.showCurrentLocation()
-		// getAddresses(api.getProfile().id) 
+		this.showCurrentLocation()
+		
+		// TEST: Using getCurrentPosition
+		// navigator.geolocation.getCurrentPosition(
+		// 	(position) => {
+		// 		this.setState({
+		// 			latitude: position.coords.latitude,
+		// 			longitude: position.coords.longitude,
+		// 			error: null
+		// 		})
+		// 	},
+		// 	(error) => this.setState({ error: error.message }),
+		// 	{ enableHighAccuracy: true, timeout: 2000, maximumAge: 1000 }
+		// )
+
+		// TEST: Using watchPosition
+		// this.watchId = navigator.geolocation.watchPosition(
+		// 	(position) => {
+		// 		this.setState({
+		// 			latitude: position.coords.latitude,
+		// 			longitude: position.coords.longitude,
+		// 			error: null
+		// 		})
+		// 	},
+		// 	(error) => this.setState({ error: error.message }),
+		// 	{ enableHighAccuracy: true, timeout: 20000, maximumAge: 1000, distanceFilter: 10 }
+		// )
+	}
+
+	componentWillUnmount() {
+		navigator.geolocation.clearWatch(this.watchId)
 	}
 
 	render() {
-		// const lat = this.state.currentLatLng.lat
-		// const lng = this.state.currentLatLng.lng
-		// const waypoint = this.props.address
-
-		// const DirectionsComponent = compose(
-		// 	withProps({
-		// 		googleMapUrl: 'https://maps.googleapis.com/maps/api/js?key=AIzaSyDNIsEsuc8FsHQJsswUcDKUd9k3sZqzk3U',
-		// 		loadingElement: <View style={{ height: `100%` }} />,
-	 //      containerElement: <View style={{ width: `100%` }} />,
-	 //      mapElement: <View style={{height: `600px`, width: `100%` }} />
-	 //    })
-
-		// 	// withScriptjs
-		// 	// withGoogleMaps
-
-		// 	lifecycle({
-		// 		componentDidMount() {
-		// 			const DirectionsService = new google.maps.Directions()
-
-		// 			DirectionsService.route({
-		// 				origin: new google.maps.LatLng({ lat: lat, lng: lng }),
-		// 				destination: api.getProfile().locatoin,
-		// 				waypoints: waypoint,
-		// 				optimizeWWayPoints: true,
-		// 				travelMode: google.maps.TravelMode.DRIVING
-		// 			}, (result, status) => {
-		// 				if (status === google.maps.DirectionsStatus.OK) {
-		// 					this.setState({
-		// 						directions: {...result}
-		// 					})
-		// 					rte = {...result.routes}
-		// 				}
-		// 			})
-		// 		}
-		// 	})
-		// )(props =>
-		// 	<GoogleMap defaultZoom={8} center={{ lat: 36.1699, lng: -115.1398 }}>
-		// 		<TrafficLayer autoUpdate />
-		// 	</GoogleMap>
-		// )
-
 		return (
 			<View style={styles.container}>
 				<MapView style={styles.map}
@@ -95,9 +93,10 @@ class Map extends Component {
 			      latitudeDelta: 0.0922,
 			      longitudeDelta: 0.0421,
 			    }}>
+
 				  <MapView.Marker
 						coordinate={coordinates[0]}
-						title={'PunchCode'}
+						title={'Current location'}
 						description={'A school for gifted children.'}
 				  />
 				  <MapView.Marker
@@ -105,17 +104,22 @@ class Map extends Component {
 						title={'In-N-Out Burger'}
 						description={'The food of the people.'}
 				  />
+
 				  <MapViewDirections 
 				  	origin={coordinates[0]}
 				  	destination={coordinates[1]}
 				  	apikey={GOOGLE_MAPS_APIKEY}
 				  	strokeWidth={3}
-				  	strokeColor='hotpink'
+				  	strokeColor='blue'
+				  	language='en'
 				  />
-
 		  	</MapView>
-
-			{/*<DirectionsComponent  />*/}
+				<Text>French Toast</Text>
+				<Text>Potatoes</Text>
+				<Text>French Fries</Text>
+				<Text>Spaghetti</Text>
+				<Text>French Bread</Text>
+				<Text>Cheeseburger</Text>
 		  </View>
 		)
 	}
@@ -127,18 +131,18 @@ const styles = StyleSheet.create({
 	container: {
     // flex: 1,
     position: 'absolute',
-    top: 20,
+    top: 0,
     left: 0,
-    bottom: 180,
+    bottom: 0,
     right: 0,
     justifyContent: 'flex-end',
     alignItems: 'center'
   },
   map: {
   	position: 'absolute',
-  	top: 0,
+  	top: 20,
   	left: 0,
-    bottom: 0,
+    bottom: 180,
     right: 0
   }
 })
