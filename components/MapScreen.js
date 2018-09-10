2f8ad661e28f5706c59fc8b7e7e69bece4066095
Victor Velasquez <victorv24@gmail.com>
2018-09-09 20:57:30 -0700

    added package json files

--- components/MapScreen.js
+++ components/MapScreen.js
@@ -100,39 +100,6 @@ class Map extends Component {
 				  	language='en'
 				  />
 		  	</MapView>
-				
-				<View style={styles.individualPickupContainer}>
-					<View style={styles.textContainer}>
-						<Text style={styles.restaurant}>Mike's Kitchen</Text>
-						<Text style={styles.address}>640 Autumn Moon Drive</Text>
-						<Text style={styles.trays}>3 trays of Meatballs</Text>
-					</View>
-					<View style={styles.iconContainer}>
-						<Entypo style={styles.icon} name='location-pin' color='white' />
-					</View>
-				</View>
-
-				<View style={styles.individualPickupContainer}>
-					<View style={styles.textContainer}>
-						<Text style={styles.restaurant}>Testaurant</Text>
-						<Text style={styles.address}>2740 Fountain Vista Lane</Text>
-						<Text style={styles.trays}>4 trays of Pasta</Text>
-					</View>
-					<View style={styles.iconContainer}>
-						<Entypo style={styles.icon} name='location-pin' color='white' />
-					</View>
-				</View>
-
-				<View style={styles.individualPickupContainer}>
-					<View style={styles.textContainer}>
-						<Text style={styles.restaurant}>Austin's Restaurant</Text>
-						<Text style={styles.address}>220 Hanley Way</Text>
-						<Text style={styles.trays}>20 trays of Wings</Text>
-					</View>
-					<View style={styles.iconContainer}>
-						<Entypo style={styles.icon} name='location-pin' color='white' />
-					</View>
-				</View>
 		  </View>
 		)
 	}
@@ -157,36 +124,5 @@ const styles = StyleSheet.create({
   	left: 0,
     bottom: 180,
     right: 0
-  },
-  restaurant: {
-		fontWeight: 'bold',
-		marginHorizontal: 10,
-		marginVertical: 5,
-		fontSize: 22,
-	},
-	address: {
-		fontSize: 20,
-		marginHorizontal: 10,
-		marginBottom: 5
-	},
-	trays: {
-		fontSize: 22,
-		margin: 10
-	},
-	textContainer: {
-		flexDirection: 'column',
-		width: 300
-	},
-	iconContainer: {
-		flexDirection: 'row',
-		justifyContent: 'center',
-		alignItems: 'center',
-		backgroundColor: '#1569C7',
-		borderRadius: 10,
-		height: 55,
-		width: 55
-	},
-	icon: {
-		fontSize: 35
-	}
+  }
 })
