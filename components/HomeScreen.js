3687e620e69aa3090361201074b0fbaee0b9d199
Mike Michaud <michaud.michael@gmail.com>
2018-09-09 21:37:10 -0700

    view wrapper to add button margin spacing for homepage

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -11,10 +11,10 @@ export default class HomeScreen extends React.Component {
 		return (
 			<View style={styles.container}>
 				<Text style={styles.header}>Pickups available for today: #</Text>
-				<Button title='My Pickups Map' onPress={() => this.props.navigation.navigate('Map')} />
-				<Button title='Manage Reports' onPress={() => this.props.navigation.navigate('Reports')} />
-				<Button title='View All Available Pickups' onPress={() => this.props.navigation.navigate('Pickups')} />
-				<Button title='Edit Profile' onPress={() => this.props.navigation.navigate('Profile')} />
+				<View style={[{margin: 5}]}><Button title='My Pickups Map' onPress={() => this.props.navigation.navigate('Map')} /></View>
+				<View style={[{margin: 5}]}><Button title='Manage Reports' onPress={() => this.props.navigation.navigate('Reports')} /></View>
+				<View style={[{margin: 5}]}><Button title='View All Available Pickups' onPress={() => this.props.navigation.navigate('Pickups')} /></View>
+				<View style={[{margin: 5}]}><Button title='Edit Profile' onPress={() => this.props.navigation.navigate('Profile')} /></View>
 			</View>
 		)
 	}
